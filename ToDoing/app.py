632b4c5f13f96276fe6a4af577a1122727bd8790
CuriousSmudge <curioussmudge@gmail.com>
2025-02-09 16:15:33 +1100

    Added background and started working on CSS and HTML

--- ToDoing/app.py
+++ ToDoing/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html'
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
